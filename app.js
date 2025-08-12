a7c5bbbac9b01e54599b3664dd1d1c7d00fbb2bc
EashitaKakkar <eashitakakkar@gmail.com>
2025-08-12 11:55:25 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+let url= "http://universities.hipolabs.com/search?name=";
+let btn= document.querySelector ("button");
+
+btn.addEventListener ("click", async () => {
+    let state= document.querySelector("input").value;
+    console.log(state);
+    let colArr= await getColleges(state);
+    show (colArr);
+})
+
+function show (colArr) {
+    let list= document.querySelector("#list");
+    list.innerText= "";
+    for (col of colArr) {
+    let li= document.createElement("li");
+    li.innerText= col.name;
+    list.appendChild(li);
+    }
+}
+
+async function getColleges(state) {
+    try{
+        let res= await axios.get(url+state+"&country=india");
+        return res.data;
+    } catch (e) {
+        console.log("error", e);
+        return [];
+    }
+    
+}
